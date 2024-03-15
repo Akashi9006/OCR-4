6c22379801ed393de018384e30a7f5641d370622
g60080zu <09zu01@gmail.com>
2024-03-15 02:10:32 +0000

    night time sleep zzzz

--- OCR_APP/App.js
+++ OCR_APP/App.js
@@ -1,23 +1,22 @@
 import { StyleSheet, Text, View} from 'react-native';
 import * as React from 'react';
-import { PaperProvider, MD3DarkTheme, Surface, Button,MD3Colors} from 'react-native-paper';
+import { PaperProvider, MD3DarkTheme, Surface, Button,withTheme} from 'react-native-paper';
 //import App from './src/App';
+
 
 const theme = {
   ...MD3DarkTheme,
 };
 export default function App() {
   return (
-    <PaperProvider theme = {theme} style={{ backgroundColor: theme.colors.primary }}>
+    <PaperProvider theme = {theme} style={styles.surface}>
+    <View style={{flex: 1,backgroundColor: "#27222b"}}>
 
-    <View>
-      <Text>hello world</Text>
-    
-
-    <Surface style={styles.surface} elevation={5}>
-      <Button icon="youtube" mode="text" onPress={() => console.log('Youtube')}>yt</Button>
+    <Surface style={styles.surface} elevation={0} >
+      <Button icon="youtube"  mode="contained" style={{ fontSize:70, width: 150, height:100, padding: 25,alignItems:"center" }} onPress={() => console.log('Youtube')}>YouTube</Button>
+      <Button icon="web" mode="contained" onPress={() => console.log('Browser')}>Browser</Button>
+      <Button icon="folder" mode="contained" onPress={() => console.log('Files')}>Files</Button>
     </Surface>  
-    <Button icon="youtube" mode="contained" onPress={() => console.log('Youtube')}>yt</Button>
 
     </View>
     </PaperProvider>
@@ -25,5 +24,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  surface: {padding: 10,height: 70,width: 70,alignItems: 'center',justifyContent: 'center',},
+  surface: {padding: 10,margin: 10 ,height: 150 ,width: 200,alignItems: 'center',justifyContent: 'space-between', alignSelf:"center", backgroundColor:"#27222b",}
 });
